1906f55075a404c5f231e7e8c835032a9e1a18f8
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-01 17:24:56 +0100

    Add get_post_data and perform_action functions. Make both topup and pay views use these functions since the views work in a similar manner.

--- wallets/thrifty_wallets/wallets/views.py
+++ wallets/thrifty_wallets/wallets/views.py
@@ -3,27 +3,45 @@ from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 
 from .models import Wallet
 
+def get_post_data(dct):
+    return (dct['id'], dct['amount'])
+
+def perform_action(wallet_id, amount, action):
+    try:
+        wallet = Wallet.objects.get(pk=wallet_id)
+    except Wallet.DoesNotExist:
+        return {'code': 404}
+    else:
+        if action == 'topup':
+            wallet.balance = wallet.balance + int(amount)
+        elif action == 'pay':
+            wallet.balance = wallet.balance - int(amount)
+
+        wallet.save()
+        return {'code': 200}
+
 @csrf_protect
 @ensure_csrf_cookie
 def topup(request):
     response = {}
 
     if request.method == 'POST':
-        wallet_id = request.POST['id']
-        amount = request.POST['amount']
-
-        try:
-            wallet = Wallet.objects.get(pk=wallet_id)
-        except Wallet.DoesNotExist:
-            response.update({'code': 404})
-        else:
-            wallet.balance = wallet.balance + int(amount)
-            wallet.save()
-            response.update({'code': 200})
+        wallet_id, amount = get_post_data(request.POST)
+        response.update(perform_action(wallet_id, amount, 'topup'))
     else:
         response.update({'status': 'ok'})
 
     return JsonResponse(response)
 
+@csrf_protect
+@ensure_csrf_cookie
 def pay(request):
-    pass
+    response = {}
+
+    if request.method == 'POST':
+        wallet_id, amount = get_post_data(request.POST)
+        response.update(perform_action(wallet_id, amount, 'pay'))
+    else:
+        response.update({'status': 'ok'})
+
+    return JsonResponse(response)
