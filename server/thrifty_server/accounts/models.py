5d10db8721135691214e6e168ebffa89354a0a6e
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-01 23:39:12 +0100

    Start thrifty_server project and accounts app. Create Accounts model.

---
+++ server/thrifty_server/accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
+
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+
+class Account(models.Model):
+    user = models.OneToOneField(User)
+    phone_number = models.CharField(validators=[phone_regex], max_length=15)
---
+++ server/thrifty_server/accounts/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def create(request):
+    pass
