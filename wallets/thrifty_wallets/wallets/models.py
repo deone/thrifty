5bd6cf4a5dea76a13f3d8d263bdbeda7407e2eb2
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-01 14:46:04 +0100

    Firstname and lastname fields shouldn't be 'blank-able'.

--- wallets/thrifty_wallets/wallets/models.py
+++ wallets/thrifty_wallets/wallets/models.py
@@ -4,8 +4,8 @@ from django.core.validators import RegexValidator
 phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 
 class Wallet(models.Model):
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     phone_number = models.CharField(validators=[phone_regex], max_length=15) # validators should be a list
     balance = models.PositiveSmallIntegerField()
     network = models.CharField(max_length=7)
