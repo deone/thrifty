bcf5f09171e5606a6262315491eea23dea9e81d9
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-01 14:00:53 +0100

    Create wallets app and Wallet model.

---
+++ wallets/thrifty_wallets/wallets/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Wallet',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=30, blank=True)),
+                ('last_name', models.CharField(max_length=30, blank=True)),
+                ('phone_number', models.CharField(max_length=15, validators=[django.core.validators.RegexValidator(regex=b'^\\+?1?\\d{9,15}$', message=b"Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")])),
+                ('balance', models.PositiveSmallIntegerField()),
+                ('network', models.CharField(max_length=7)),
+            ],
+        ),
+    ]
